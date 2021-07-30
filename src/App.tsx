e6d7842fce0d598439a2d21c31379a374f2a3446
Welton Felix <contato.weltonf@gmail.com>
2021-07-30 15:27:33 -0300

    create main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { IoIosArrowDown as ArrowDown } from "react-icons/io";
+
 import "./global.css";
+import "./App.css";
 
 function App() {
-  return <h1>Kindred</h1>;
+  return (
+    <>
+    <main id="title">
+      <h1>Kindred</h1>
+      <h3>Octavia Butler</h3>
+      <a href="#chapter-1">
+        <p>Começar</p>
+        <ArrowDown color="#ffffff" size="32"/>
+      </a>
+    </main>
+  </>
+  );
 }
 
 export default App;
