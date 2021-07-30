8d92c4fdd0ca9704d0cb3449b187f2e322dac404
Welton Felix <contato.weltonf@gmail.com>
2021-07-30 14:45:56 -0300

    Reset CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
+import "./global.css";
 
 function App() {
-  return (
-    <h1>Kindred</h1>
-  );
+  return <h1>Kindred</h1>;
 }
 
 export default App;
